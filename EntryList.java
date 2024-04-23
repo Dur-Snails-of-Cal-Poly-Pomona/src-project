030e87e7550d1b5873c866fb814f980eb25b8a0a
somethingstamo <insertuniqueemail@gmail.com>
2024-04-23 13:33:07 -0700

    Adding javadoc comments to EntryList.java

--- EntryList.java
+++ EntryList.java
@@ -10,14 +10,26 @@ public class EntryList {
     private LinkedList<Entry> allEntries;
     private String filename;
 
+    /**
+     * Returns the name of the file this EntryList was loaded from/saves to
+     * @return the name of the file this EntryList was loaded from/saves to
+     */
     public String getFilename() {
         return filename;
     }
 
+    /**
+     * Returns the list of all entries in this EntryList
+     * @return the list of all entries in this EntryList
+     */
     public LinkedList<Entry> getAllEntries() {
         return allEntries;
     }
 
+    /**
+     * Creates a new EntryList loaded from the specified file, or empty if no file is found
+     * @param filenameToLoad the name of the file to load the EntryList from
+     */
     EntryList(String filenameToLoad) {
         this.filename = filenameToLoad;
         // Load allEntries from file, make new allEntries if none found
@@ -33,6 +45,11 @@ public class EntryList {
         }
     }
 
+    /**
+     * Loads the EntryList from the specified file
+     * @param filenameToLoad the name of the file to load the EntryList from
+     * @return true if the file successfully loaded, false otherwise
+     */
     @SuppressWarnings("unchecked")
     public boolean loadFromFile(String filenameToLoad) {
         try {
@@ -55,6 +72,10 @@ public class EntryList {
         return true;
     }
 
+    /**
+     * Saves this EntryList to the/a file specified by filename
+     * @return true if the file was successfully saved, false otherwise
+     */
     public boolean saveToFile() {
         try {
             FileOutputStream fos = new FileOutputStream(filename);
@@ -69,6 +90,12 @@ public class EntryList {
         return true;
     }
 
+    /**
+     * Adds a new entry to this EntryList's list of entries
+     * @param date the date of the new entry
+     * @param numVisitors the number of visitors in the new entry
+     * @param donations the donations in the new entry
+     */
     public void addEntry(LocalDate date, int numVisitors, ResizableArrayBag<String> donations) {
         allEntries.add(new Entry(date, numVisitors, donations));
     }
